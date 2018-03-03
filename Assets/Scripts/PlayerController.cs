d8483d749e6685b6edefd76279ee19c60c5043bd
StavenX <edvin_myhren@hotmail.com>
2018-03-03 17:59:57 +0100

    - added player prefab - added playercontroller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var h = Input.GetAxis("Horizontal") * 150 * Time.deltaTime * 150f;
+        var v = Input.GetAxis("Vertical") * 150 * Time.deltaTime * 3.0f;
+
+        GetComponent<Rigidbody2D>().AddForce(new Vector3(h, v, 0));
+
+    }
+}
