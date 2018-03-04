e310b96342431126c304c4d32d5ce0906ba608d9
StavenX <edvin_myhren@hotmail.com>
2018-03-04 15:30:22 +0100

    Added start menu + imported asset
    - added start menu (scene)
    - imported a free asset (textmesh pro), being used in menu text boxes, text materials etc
    - added options menu (inside menu scene)
    - added working play-button
    - added working quit-button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+    // Loads the next scene when called
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Open the Options-menu (currently non-existent)
+    public void openOptions()
+    {
+        Debug.Log("sonpi pls help me code here");
+    }
+
+    // Quits the game when called
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
